494bea9f9f2c16b4c38e647e1bae64aecc5a519d
codewithalimizbani <alimizbani.js@gmail.com>
2023-11-12 11:07:59 +0330

    addd assets and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,46 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+import "./globals.css";
 
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+const spaceGrotest = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-space-grotesk",
+});
+
+export const metadata: Metadata = {
+  title: "DevFlow",
+  description: "A Community for Developers",
+  icons: {
+    icon: "/assests/images/site-logo.svg",
+  },
+};
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotest.variable}`}>
+          <h1 className="h1-bold">this is i peace of tst</h1>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
